fe2e60c85bd28eb79d94fee2e39ed9b6558bcb04
Harsha Nalluru <sanallur@microsoft.com>
2023-04-20 17:34:34 +0000

    mem usage

--- sdk/test-utils/perf/src/managerProgram.ts
+++ sdk/test-utils/perf/src/managerProgram.ts
@@ -95,10 +95,10 @@ export class ManagerPerfProgram implements PerfProgram {
       `Completed ${totalOperations.toLocaleString(undefined, {
         maximumFractionDigits: 0,
       })} ` +
-        `operations in a weighted-average of ` +
-        `${formatNumber(weightedAverage, 4)}s ` +
-        `(${formatNumber(operationsPerSecond, 4)} ops/s, ` +
-        `${formatNumber(secondsPerOperation, 4)} s/op)`
+      `operations in a weighted-average of ` +
+      `${formatNumber(weightedAverage, 4)}s ` +
+      `(${formatNumber(operationsPerSecond, 4)} ops/s, ` +
+      `${formatNumber(secondsPerOperation, 4)} s/op)`
     );
   }
 
@@ -107,11 +107,12 @@ export class ManagerPerfProgram implements PerfProgram {
     const currentCompleted = totalCompleted - this.lastCompleted;
     const averageCompleted = this.getOperationsPerSecond(parallels);
     const elapsedTime = formatDuration(new Date().getTime() - this.startMillis);
+    const { arrayBuffers, rss, heapUsed } = process.memoryUsage()
 
     this.lastCompleted = totalCompleted;
 
     console.log(
-      `${elapsedTime}\t\t${currentCompleted}\t\t${totalCompleted}\t\t${averageCompleted.toFixed(2)}`
+      `${elapsedTime}\t\t${currentCompleted}\t\t${totalCompleted}\t\t${averageCompleted.toFixed(2)}\t\t${arrayBuffers}\t\t${rss}\t\t${heapUsed}`
     );
   }
 
@@ -159,11 +160,10 @@ export class ManagerPerfProgram implements PerfProgram {
     // of operations running.
     const millisecondsToLog = Number(this.parsedOptions["milliseconds-to-log"].value);
     console.log(
-      `\n=== ${title} mode, iteration ${iterationIndex + 1}. Logs every ${
-        millisecondsToLog / 1000
+      `\n=== ${title} mode, iteration ${iterationIndex + 1}. Logs every ${millisecondsToLog / 1000
       }s ===`
     );
-    console.log(`ElapsedTime\tCurrent\t\tTotal\t\tAverage`);
+    console.log(`ElapsedTime\tCurrent\t\tTotal\t\tAverage\t\tArrayBuffers\t\tRSS\t\tHEAPUSED`);
 
     let done = false;
 
